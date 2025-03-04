aedac6b1e9aa5192b7e9df3d293161e6bdb3c774
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-04 15:48:26 +0900

    新着一覧PC

--- front-page.php
+++ front-page.php
@@ -3,34 +3,42 @@
 <div class="logo"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/common/logo_big.svg" alt="" width="" height="" loading="lazy" decoding="async"></div>
 
 
-<section class="top-news">
-    <div class="top-news__inner inner">
-        <div class="top-news__title">
-            <h2 class="section-title txt"><span>news</span>お知らせ</h2>
-        </div>
-        <ul class="top-news__items">
-            <?php $args = array(
-            'post_type' => 'post',
-            'posts_per_page' => 3,
-            'order' => 'DESC',
-            );
-            $the_query = new WP_Query($args);
-            if ($the_query->have_posts()):
-            while($the_query->have_posts()) :
-            $the_query->the_post(); ?>
-                <li class="top-news__item">
-                    <div class="top-news__meta">
-                        <time datetime="<?php the_time('c'); ?>" class="top-news__time"><?php the_time('Y.n.j'); ?></time>
-                    </div>
-                    <div class="news__contents">
-                        <p class="top-news__txt txt"><?php the_content(); ?></p>
-                    </div>
-                </li>
-            <?php endwhile; endif; ?>
-            <?php wp_reset_postdata(); ?>
-        </ul>
-        <div class="top-news__btn">
-            <a href="<?php echo esc_url(home_url("news")); ?>" class="btn btn--contact">more</a>
+<section class="news">
+    <div class="news__inner inner">
+        <div class="news__body">
+            <div class="news__top">
+                <h2 class="news__title">新着情報<span>TOPICS</span></h2>
+                <div class="news__btn"><a href="" class="news__link">一覧を見る</a></div>
+            </div>
+            <ul class="news__items">
+                <?php $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => 3,
+                'order' => 'DESC',
+                );
+                $the_query = new WP_Query($args);
+                if ($the_query->have_posts()):
+                while($the_query->have_posts()) :
+                $the_query->the_post(); ?>
+                    <li class="news__item">
+                        <div class="news__meta">
+                            <time datetime="<?php the_time('c'); ?>" class="news__time"><?php the_time('Y.n.j'); ?></time>
+                        </div>
+                        <div class="news__contents">
+                            <p class="news__txt txt"><?php
+                                if ( mb_strlen( $post->post_title, 'UTF-8' ) > 28 ) {
+                                $title = mb_substr( $post->post_title, 0, 28, 'UTF-8' );
+                                    echo $title . '…';
+                                } else {
+                                    echo $post->post_title;
+                                }
+                                ?></p>
+                        </div>
+                    </li>
+                <?php endwhile; endif; ?>
+                <?php wp_reset_postdata(); ?>
+            </ul>
+        
         </div>
     </div>
 </section>
