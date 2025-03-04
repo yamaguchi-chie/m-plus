644ba64692d4380a6e1e73e3a9adf7909d1ac36e
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-04 18:30:49 +0900

    メニューカードPC

--- front-page.php
+++ front-page.php
@@ -61,6 +61,110 @@
     </div>
 </section>
 
+<section class="top-menu">
+    <div class="top-menu__inner inner">
+        <div class="top-menu__top">
+            <h2 class="top-menu__title"></h2>
+            <p class="top-menu__en"></p>
+        </div>
+        <p class="top-menu__catch"></p>
+        <div class="top-menu__body">
+            <ul class="menu-card-list">
+                <li class="menu-card-list__item menu-card">
+                    <div class="menu-card__top">
+                        <h3 class="menu-card__title"><span>使いにくい・せまい・古くなってきた</span>キッチンリフォーム</h3>
+                    </div>
+                    <div class="menu-card__img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/menu/1.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                    </div>
+                    <div class="menu-card__contents">
+                        <h4 class="menu-card__subTl">工事費込［総額］</h4>
+                        <div class="menu-card__meta">
+                            <p class="menu-card__price">45</p>
+                            <div class="menu-card__txt"><span>（税込）</span>万円～</div>
+                        </div>
+                    </div>
+                </li>
+                <li class="menu-card-list__item menu-card">
+                    <div class="menu-card__top">
+                        <h3 class="menu-card__title"><span>カビが気になる、節水したい、浴室が寒い</span>浴室リフォーム</h3>
+                    </div>
+                    <div class="menu-card__img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/menu/2.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                    </div>
+                    <div class="menu-card__contents">
+                        <h4 class="menu-card__subTl">工事費込［総額］</h4>
+                        <div class="menu-card__meta">
+                            <p class="menu-card__price">42</p>
+                            <div class="menu-card__txt"><span>（税込）</span>万円～</div>
+                        </div>
+                    </div>
+                </li>
+                <li class="menu-card-list__item menu-card">
+                    <div class="menu-card__top">
+                        <h3 class="menu-card__title"><span>汚れが目立つ・和式から洋式に変えたい</span>トイレリフォーム</h3>
+                    </div>
+                    <div class="menu-card__img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/menu/3.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                    </div>
+                    <div class="menu-card__contents">
+                        <h4 class="menu-card__subTl">工事費込［総額］</h4>
+                        <div class="menu-card__meta">
+                            <p class="menu-card__price">14</p>
+                            <div class="menu-card__txt"><span>（税込）</span>万円～</div>
+                        </div>
+                    </div>
+                </li>
+                <li class="menu-card-list__item menu-card">
+                    <div class="menu-card__top">
+                        <h3 class="menu-card__title"><span>さびが気になる、使い勝手が悪い</span>洗面リフォーム</h3>
+                    </div>
+                    <div class="menu-card__img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/menu/4.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                    </div>
+                    <div class="menu-card__contents">
+                        <h4 class="menu-card__subTl">工事費込［総額］</h4>
+                        <div class="menu-card__meta">
+                            <p class="menu-card__price">12</p>
+                            <div class="menu-card__txt"><span>（税込）</span>万円～</div>
+                        </div>
+                    </div>
+                </li>
+                <li class="menu-card-list__item menu-card">
+                    <div class="menu-card__top">
+                        <h3 class="menu-card__title"><span>張り替えしたい・和室を洋室に変えたい</span>内装リフォーム</h3>
+                    </div>
+                    <div class="menu-card__img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/menu/5.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                    </div>
+                    <div class="menu-card__contents">
+                        <h4 class="menu-card__subTl">工事費込［総額］</h4>
+                        <div class="menu-card__meta">
+                            <p class="menu-card__price">35</p>
+                            <div class="menu-card__txt"><span>（税込）</span>万円～</div>
+                        </div>
+                    </div>
+                </li>
+                <li class="menu-card-list__item menu-card">
+                    <div class="menu-card__top">
+                        <h3 class="menu-card__title"><span>壁のひび割れ、遮熱工事がしたい</span>外装リフォーム</h3>
+                    </div>
+                    <div class="menu-card__img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/menu/6.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                    </div>
+                    <div class="menu-card__contents">
+                        <h4 class="menu-card__subTl">工事費込［総額］</h4>
+                        <div class="menu-card__meta">
+                            <p class="menu-card__price">70</p>
+                            <div class="menu-card__txt"><span>（税込）</span>万円～</div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
 <?php get_template_part('template/contact'); ?>
 
 <?php get_footer(); ?>
