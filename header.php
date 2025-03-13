1e22f950d9dd1fa66920b05a977900b9fb3211ea
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-13 17:44:34 +0900

    PC

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
   <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body>
   <?php wp_body_open(); ?>
 
     <!-- header  -->
---
+++ page-privacy-policy.php
@@ -0,0 +1,135 @@
+
+<?php get_header(); ?>
+<?php
+$mainviews = [
+    [
+        'title' => 'プライバシーポリシー',
+        'en' => 'Privacy Policy',
+    ],
+];
+?>
+<section class="sub-mv <?php if (is_page('privacy-policy')) {
+    echo 'sub-mv--privacy';
+} ?>">
+<?php get_template_part('template/sub-mv', null, ['mainviews' => $mainviews]); ?>
+</section>
+
+<section class="privacy-policy">
+    <div class="privacy-policy__inner inner">
+        <h2 class="privacy-policy__title">プライバシーポリシー<span>[ 個人情報保護方針 ]</span></h2>
+        <div class="privacy-policy__body">
+            <p class="privacy-policy__catch">エムプラス株式会社<span>（以下「当社」といいます）</span>は、本ウェブサイト上で提供するサービス<span>（以下「本サービス」といいます）</span>におけるユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー<span>（以下「本ポリシー」といいます）</span>を定めます。</p>
+            <dl class="privacy-policy__label">
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第１条（個人情報）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">「個人情報」とは、個人情報保護法にいう「個人情報」を指すものとし、生存する個人に関する情報であって、当該情報に含まれる氏名、生年月日、住所、電話番号、連絡先その他の記述等により特定の個人を識別できる情報及び容貌、指紋、声紋にかかるデータ、及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第２条（個人情報の収集方法）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">当社は、ユーザーが利用登録をする際に氏名、生年月日、住所、電話番号、メールアドレス、銀行口座番号、クレジットカード番号、運転免許証番号などの個人情報をお尋ねすることがあります。また、ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を、当社の提携先（情報提供元、広告主、広告配信先などを含みます。以下｢提携先｣といいます。）などから収集することがあります。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第３条（個人情報を収集・利用する目的）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">当社が個人情報を収集・利用する目的は，以下のとおりです。</p>
+                        <ul class="privacy-policy__list">
+                            <li class="privacy-policy__list-txt">［１］当社サービスの提供・運営のため</li>
+                            <li class="privacy-policy__list-txt">［２］ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</li>
+                            <li class="privacy-policy__list-txt">［３］更新情報、キャンペーン等及び当社が提供する他のサービスの案内のメールを送付するため</li>
+                            <li class="privacy-policy__list-txt">［４］メンテナンス、重要なお知らせなど必要に応じたご連絡のため</li>
+                            <li class="privacy-policy__list-txt">［５］利用規約に違反したユーザーや、不正・不当な目的でサービスを利用しようとするユーザーの特定をし、ご利用をお断りするため</li>
+                            <li class="privacy-policy__list-txt">［６］ユーザーにご自身の登録情報の閲覧や変更、削除、ご利用状況の閲覧を行っていただくため</li>
+                            <li class="privacy-policy__list-txt">［７］有料サービスにおいて、ユーザーに利用料金を請求するため</li>
+                            <li class="privacy-policy__list-txt">［８］上記の利用目的に付随する目的</li>
+                        </ul>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第４条（利用目的の変更）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">当社は、利用目的が変更前と関連性を有すると合理的に認められる場合に限り、個人情報の利用目的を変更するものとします。<br>利用目的の変更を行った場合には、変更後の目的について、当社所定の方法により、ユーザーに通知し、または本ウェブサイト上に公表するものとします。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第５条（個人情報の第三者提供）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">当社は、次に掲げる場合を除いて，あらかじめユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。<br>ただし、個人情報保護法その他の法令で認められる場合を除きます。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第６条（個人情報の開示）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">当社は、本人から個人情報の開示を求められたときは、本人に対し、遅滞なくこれを開示します。ただし、開示することにより次のいずれかに該当する場合は、その全部または一部を開示しないこともあり、開示しない決定をした場合には、その旨を遅滞なく通知します。<br>前項の定めにかかわらず、履歴情報および特性情報などの個人情報以外の情報については、原則として開示いたしません。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第７条（個人情報の訂正および削除）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">ユーザーは、当社の保有する自己の個人情報が誤った情報である場合には、当社が定める手続きにより、当社に対して個人情報の訂正、追加または削除（以下「訂正等」といいます。）を請求することができます。<br>当社は、ユーザーから前項の請求を受けてその請求に応じる必要があると判断した場合には、遅滞なく、当該個人情報の訂正等を行うものとします。<br>当社は、前項の規定に基づき訂正等を行った場合、または訂正等を行わない旨の決定をしたときは遅滞なく、これをユーザーに通知します。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第８条（個人情報の利用停止等）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">当社は、本人から、個人情報が、利用目的の範囲を超えて取り扱われているという理由、または不正の手段により取得されたものであるという理由により、その利用の停止または消去（以下「利用停止等」といいます。）を求められた場合には、遅滞なく必要な調査を行います。<br>前項の調査結果に基づき、その請求に応じる必要があると判断した場合には、遅滞なく、当該個人情報の利用停止等を行います。<br>当社は、前項の規定に基づき利用停止等を行った場合、または利用停止等を行わない旨の決定をしたときは、遅滞なく、これをユーザーに通知します。<br>上記にかかわらず、利用停止等に多額の費用を有する場合その他利用停止等を行うことが困難な場合であって、ユーザーの権利利益を保護するために必要なこれに代わるべき措置をとれる場合は、この代替策を講じるものとします。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第９条（プライバシーポリシーの変更）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">本ポリシーの内容は、法令その他本ポリシーに別段の定めのある事項を除いて、ユーザーに通知することなく、変更することができるものとします。当社が別途定める場合を除いて、変更後のプライバシーポリシーは、本ウェブサイトに掲載したときから効力を生じるものとします。</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__item">
+                    <dt class="privacy-policy__main">
+                        <p class="privacy-policy__subTl">第１０条（お問い合わせ窓口）</p>
+                    </dt>
+                    <dd class="privacy-policy__contents">
+                        <p class="privacy-policy__text">本プライバシーポリシーに関するお問い合わせは、下記までお願いいたします。</p>
+                    </dd>
+                </div>
+            </dl>
+            <dl class="privacy-policy__bottom">
+                <div class="privacy-policy__adress">
+                    <dt class="privacy-policy__adressTl">《 住 所 》</dt>
+                    <dd class="privacy-policy__adress-contents">
+                        <p>〒462-0825 愛知県名古屋市北区大曽根3-10-7</p>
+                        <p> TEL:0120-857-735　 FAX:052-508-5032</p>
+                    </dd>
+                </div>
+                <div class="privacy-policy__adress">
+                    <dt class="privacy-policy__adressTl">《 社 名 》</dt>
+                    <dd class="privacy-policy__adress-contents">
+                        <p>エムプラス株式会社</p>
+                        <p>代表取締役　後 藤　亮 平</p>
+                    </dd>
+                </div>
+            </dl>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
