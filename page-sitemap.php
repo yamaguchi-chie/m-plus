b1564f3ad5b0042bc98303c69562e6c9c83b90ab
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-14 14:39:56 +0900

    PC/SP

---
+++ page-sitemap.php
@@ -0,0 +1,75 @@
+<?php get_header(); ?>
+<?php
+$mainviews = [
+    [
+        'title' => 'サイトマップ',
+        'en' => 'Site map',
+    ],
+];
+?>
+<section class="sub-mv <?php if (is_page('sitemap')) {
+    echo 'sub-mv--privacy';
+} ?>">
+<?php get_template_part('template/sub-mv', null, ['mainviews' => $mainviews]); ?>
+</section>
+
+<section class="sitemap">
+    <div class="sitemap__inner inner">
+        <ul class="sitemap__list">
+            <li class="sitemap__item">
+                <h2 class="sitemap__subTl sitemap__subTl--space">T  O  P</h2>
+                <ul class="sitemap__contents sitemap__contents--gap">
+                    <li class="sitemap__link sitemap__link--circle"><a href="<?php echo esc_url(home_url("reason")); ?>">top</a></li>
+                    <li class="sitemap__link sitemap__link--circle"><a href="<?php echo esc_url(home_url("reason")); ?>">新着情報一覧</a></li>
+                </ul>
+            </li>
+            <li class="sitemap__item">
+                <h2 class="sitemap__subTl">選ばれる理由</h2>
+                <ul class="sitemap__contents">
+                    <li class="sitemap__link"><a href="<?php echo esc_url(home_url("reason")); ?>">選ばれる理由</a></li>
+                </ul>
+            </li>
+            <li class="sitemap__item">
+                <h2 class="sitemap__subTl">リフォームメニュー</h2>
+                <ul class="sitemap__contents">
+                    <li class="sitemap__nav-item">
+                        <ul>
+                            <li class="sitemap__link"><a href="<?php echo esc_url(home_url("kitchen")); ?>">キッチンリフォーム</a></li>
+                            <li class="sitemap__link"><a href="<?php echo esc_url(home_url("bath")); ?>">浴室リフォーム</a></li>
+                            <li class="sitemap__link"><a href="<?php echo esc_url(home_url("toilet")); ?>">トイレリフォーム</a></li>
+                        </ul>
+                    </li>
+                    <li class="sitemap__nav-item">
+                    <ul>
+                        <li class="sitemap__link"><a href="<?php echo esc_url(home_url("washroom")); ?>">洗面リフォーム</a></li>
+                        <li class="sitemap__link"><a href="<?php echo esc_url(home_url("interior")); ?>">内装リフォーム</a></li>
+                        <li class="sitemap__link"><a href="<?php echo esc_url(home_url("exterior")); ?>">外装リフォーム</a></li>
+                    </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="sitemap__item">
+                <h2 class="sitemap__subTl">リフォームの流れ</h2>
+                <ul class="sitemap__contents">
+                    <li class="sitemap__link"><a href="<?php echo esc_url(home_url("reason")); ?>">ご依頼までの流れ</a></li>
+                </ul>
+            </li>
+            <li class="sitemap__item">
+                <h2 class="sitemap__subTl">会社情報</h2>
+                <ul class="sitemap__contents sitemap__contents--gap">
+                    <li class="sitemap__link"><a href="<?php echo esc_url(home_url("company")); ?>">代表挨拶</a></li>
+                    <li class="sitemap__link"><a href="<?php echo esc_url(home_url("company")); ?>">経営理念</a></li>
+                    <li class="sitemap__link"><a href="<?php echo esc_url(home_url("company")); ?>">会社概要</a></li>
+                    <li class="sitemap__link"><a href="<?php echo esc_url(home_url("company")); ?>">アクセス</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="sitemap__bottom">
+            <li class="sitemap__link sitemap__link--circle"><a href="<?php echo esc_url(home_url("contact")); ?>">お問い合わせ</a></li>
+            <li class="sitemap__link sitemap__link--circle"><a href="<?php echo esc_url(home_url("privacy-policy")); ?>">プライバシーポリシー</a></li>
+            <li class="sitemap__link sitemap__link--circle"><a href="<?php echo esc_url(home_url("sitemap")); ?>">サイトマップ</a></li>
+        </ul>
+    </div>
+</section>
+
+<?php get_footer(); ?>
