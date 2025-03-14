79c9f7e142c902ecaf67527476df1ef661f3f9a1
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-14 11:52:23 +0900

    cta　PC

---
+++ template/cta.php
@@ -0,0 +1,12 @@
+<ul class="cta-btn-list">
+    <li class="cat-btn-list__item">
+        <a href="" class="cta-btn cta-btn--tel">
+            <div class="cta-btn__contents">
+                電話で相談する
+                <p class="cta-btn__tel">0120-857-735</p>
+                <p class="cta-btn__note">【受付】<span>平日9：00〜18：00</span></p>
+            </div>
+        </a>
+    </li>
+    <li class="cat-btn-list__item"><a href="" class="cta-btn cta-btn--mail"><div class="cta-btn__contents"><span>メールでの<br>お問い合わせ</span></div></a></li>
+</ul>
