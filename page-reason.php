dee452c4cddd539d893b83d4f5fbea632d1f9833
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-14 16:38:09 +0900

    相談ください。PC

--- page-reason.php
+++ page-reason.php
@@ -23,50 +23,68 @@ $mainviews = [
     </div>
 </section>
 
-<section class="reason">
-    <div class="reason__inner inner">
-        <div class="reason__top fade-in">
-            <h2 class="reason__title">エムプラスが選ばれる３つの理由</h2>
+<section class="sub-reason">
+    <div class="sub-reason__inner inner">
+        <div class="sub-reason__top fade-in">
+            <h2 class="sub-reason__title">エムプラスが選ばれる３つの理由</h2>
         </div>
-        <div class="reason__body">
-            <ul class="reason__list">
-                <li class="reason__item">
-                    <div class="reason__photo"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/photo1.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="reason__contents">
-                        <div class="reason__subTl">
-                            <img class="reason__point" src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/point1.webp" alt="" width="" height="" loading="lazy" decoding="async">
-                            <h3 class="reason__h3">信頼</h3>
+        <div class="sub-reason__body">
+            <ul class="sub-reason__list">
+                <li class="sub-reason__item">
+                    <div class="sub-reason__photo"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/photo1.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                    <div class="sub-reason__contents">
+                        <div class="sub-reason__subTl">
+                            <img class="sub-reason__point" src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/point1.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                            <h3 class="sub-reason__h3">信頼</h3>
                         </div>
-                        <p class="reason__catch">どんな小さなことでも気軽に相談OK！</p>
-                        <p class="reason__text">「こんなことで相談しても大丈夫かな？」と迷うようなことも、どうぞ気にせずお話しください！<br>ドアのガタつきや水漏れ修理、壁紙のちょっとした張り替えなど、 どんな小さなお困りごとでも大歓迎！<br>「こうしたいなぁ…」というぼんやりしたイメージでも大丈夫です。お客様と一緒に考えながら、 ピッタリの解決策をご提案します。</p>
+                        <p class="sub-reason__catch">どんな小さなことでも気軽に相談OK！</p>
+                        <p class="sub-reason__text">「こんなことで相談しても大丈夫かな？」と迷うようなことも、どうぞ気にせずお話しください！<br>ドアのガタつきや水漏れ修理、壁紙のちょっとした張り替えなど、 どんな小さなお困りごとでも大歓迎！<br>「こうしたいなぁ…」というぼんやりしたイメージでも大丈夫です。お客様と一緒に考えながら、 ピッタリの解決策をご提案します。</p>
                     </div>
-                    <div class="reason__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/deco1.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                    <div class="sub-reason__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/deco1.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
                 </li>
-                <li class="reason__item reason__item--reserve">
-                    <div class="reason__photo"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/photo2.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="reason__contents">
-                        <div class="reason__subTl">
-                            <img class="reason__point" src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/point2.webp" alt="" width="" height="" loading="lazy" decoding="async">
-                            <h3 class="reason__h3">速さ</h3>
+                <li class="sub-reason__item reason__item--reserve">
+                    <div class="sub-reason__photo"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/photo2.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                    <div class="sub-reason__contents">
+                        <div class="sub-reason__subTl">
+                            <img class="sub-reason__point" src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/point2.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                            <h3 class="sub-reason__h3">速さ</h3>
                         </div>
-                        <p class="reason__catch">スピード重視、丁寧に迅速対応します！</p>
-                        <p class="reason__text">「すぐに直したい！」「この日までに終わるかな？」そんなときもお任せください！<br>地域密着だからこそ、 スピーディーに対応できるのが強みです。<br>でも、スピードだけが売りではありません。<br>しっかり丁寧に作業して、ずっと安心できる仕上がり にします。「頼んでよかった！」と思っていただけるよう、心を込めて対応いたします。</p>
+                        <p class="sub-reason__catch">スピード重視、丁寧に迅速対応します！</p>
+                        <p class="sub-reason__text">「すぐに直したい！」「この日までに終わるかな？」そんなときもお任せください！<br>地域密着だからこそ、 スピーディーに対応できるのが強みです。<br>でも、スピードだけが売りではありません。<br>しっかり丁寧に作業して、ずっと安心できる仕上がり にします。「頼んでよかった！」と思っていただけるよう、心を込めて対応いたします。</p>
                     </div>
-                    <div class="reason__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/deco2.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                    <div class="sub-reason__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/deco2.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
                 </li>
-                <li class="reason__item">
-                    <div class="reason__photo"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/photo3.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="reason__contents">
-                        <div class="reason__subTl">
-                            <img class="reason__point" src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/point3.webp" alt="" width="" height="" loading="lazy" decoding="async">
-                            <h3 class="reason__h3">金額</h3>
+                <li class="sub-reason__item">
+                    <div class="sub-reason__photo"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/photo3.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                    <div class="sub-reason__contents">
+                        <div class="sub-reason__subTl">
+                            <img class="sub-reason__point" src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/point3.webp" alt="" width="" height="" loading="lazy" decoding="async">
+                            <h3 class="sub-reason__h3">金額</h3>
                         </div>
-                        <p class="reason__catch">ムリなくできる、安心価格のリフォーム！</p>
-                        <p class="reason__text">「リフォームって高そう…」<br>そんな不安、ありませんか？<br>私たちは、 本当に必要な工事だけを、お手頃な価格でご提案。 ムリな営業は絶対にしません！<br>ご予算に合わせて、 できること・できないことを正直にお伝えしながら、一番いい方法を一緒に考えます。<br>「思っていたより気軽にできた！」と<br>喜んでいただけるよう、全力でサポートします。</p>
+                        <p class="sub-reason__catch">ムリなくできる、安心価格のリフォーム！</p>
+                        <p class="sub-reason__text">「リフォームって高そう…」<br>そんな不安、ありませんか？<br>私たちは、 本当に必要な工事だけを、お手頃な価格でご提案。 ムリな営業は絶対にしません！<br>ご予算に合わせて、 できること・できないことを正直にお伝えしながら、一番いい方法を一緒に考えます。<br>「思っていたより気軽にできた！」と<br>喜んでいただけるよう、全力でサポートします。</p>
                     </div>
-                    <div class="reason__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/deco3.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                    <div class="sub-reason__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/deco3.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
                 </li>
             </ul>
+        </div>
+    </div>
+</section>
+
+<section class="sub-reason-bottom">
+    <div class="sub-reason-bottom__inner inner">
+        <div class="sub-reason-bottom__top">
+            <h2 class="sub-reason-bottom__title"><span>【お見積もり無料】</span>まずはお気軽にご相談ください<span>！</span></h2>
+        </div>
+        <div class="sub-reason-bottom__body">
+            <div class="sub-reason-bottom__box">
+                <div class="sub-reason-bottom__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/bottom-img.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                <div class="sub-reason-bottom__contents">
+                    <p class="sub-reason-bottom__text">リフォームやリノベーションは、大きな決断。<br>大変そうに感じるけど、実は「自分の理想を叶えるための冒険」みたいなもの。だけど一人で悩まなくて大丈夫です。理想を一緒にカタチにするために、私たちは全力で寄り添います。どんな小さな疑問や不安も、まずは気軽にお話しください。お客様の夢を、一緒にカタチにするお手伝いをさせてください。</p>
+                    <p class="sub-reason-bottom__catch">理想の暮らしへの一歩を、一緒に踏み出しましょう！</p>
+                </div>
+                <div class="sub-reason-bottom__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/bottom-deco.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+            </div>
         </div>
     </div>
 </section>
