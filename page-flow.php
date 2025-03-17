5e8c7248c12bebc6e9697eb684c218725ff20522
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-17 17:26:44 +0900

    トップPC

---
+++ page-flow.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<section class="sub-flow-top">
+    <div class="sub-flow-top__inner inner">
+        <div class="sub-flow-top__contents">
+            <h3 class="sub-flow-top__title">お客様に寄り添う家づくりで、理想をカタチにする安心リフォームの流れ。</h3>
+            <p class="sub-flow-top__text">まずは、どんな小さなことでも構いません。夢を膨らませるのが第一歩です！<br>「もっと明るいリビングで家族と過ごしたい」「キッチンを広くして、友達と料理を楽しみたい」<br>「子どもたちがのびのび遊べる空間が欲しい」など、お客様の理想の暮らしをお聞かせください。<br>私たちは、その想いにしっかり寄り添いながら、一緒に未来の家を描いていきます。<br>ご相談をお聞きした上で、現地調査を行いながら最適なプランをご提案いたします。<br>自社施工なので、お引渡しからアフターフォローまで安心してお任せいただけます。</p>
+        </div>
+        <div class="sub-flow-top__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/flow-img.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+    </div>
+</section>
+
+<?php get_template_part('template/contact-catch'); ?>
+<?php get_template_part('template/cta'); ?>
+<?php get_footer(); ?>
---
+++ template/contact-catch.php
@@ -0,0 +1,17 @@
+<section class="sub-reason-bottom">
+    <div class="sub-reason-bottom__inner inner">
+        <div class="sub-reason-bottom__top">
+            <h2 class="sub-reason-bottom__title"><span>【お見積もり無料】</span>まずはお気軽にご相談ください<span>！</span></h2>
+        </div>
+        <div class="sub-reason-bottom__body">
+            <div class="sub-reason-bottom__box">
+                <div class="sub-reason-bottom__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/bottom-img.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                <div class="sub-reason-bottom__contents">
+                    <p class="sub-reason-bottom__text">リフォームやリノベーションは、大きな決断。<br>大変そうに感じるけど、実は「自分の理想を叶えるための冒険」みたいなもの。だけど一人で悩まなくて大丈夫です。理想を一緒にカタチにするために、私たちは全力で寄り添います。どんな小さな疑問や不安も、まずは気軽にお話しください。お客様の夢を、一緒にカタチにするお手伝いをさせてください。</p>
+                    <p class="sub-reason-bottom__catch">理想の暮らしへの一歩を、一緒に踏み出しましょう！</p>
+                </div>
+                <div class="sub-reason-bottom__deco"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reason/bottom-deco.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+            </div>
+        </div>
+    </div>
+</section>
