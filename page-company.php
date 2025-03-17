9df24f05538e59a32ba40ad015894839e62c4ea7
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-17 16:49:31 +0900

    会社情報SP

--- page-company.php
+++ page-company.php
@@ -17,8 +17,8 @@ $mainviews = [
     <div class="sub-company__inner inner">
         <div class="sub-company__body">
             <div class="sub-company__contents">
-                <h2 class="sub-company__title">お客様の夢とこだわりをカタチにする、<br>まごころリフォーム・リノベーションの会社です。</h2>
-                <p class="sub-company__txt">名古屋市や春日井市、小牧市を中心とした地域密着の<br>リフォーム・リノベーション会社・エムプラスです。<br>お客様との出会いとご縁、職人さんとの絆を大切にした<br>まごころを込めたリフォームを実現します。</p>
+                <h2 class="sub-company__title">お客様の夢とこだわりをカタチにする、<br class="u-desktop">まごころリフォーム・リノベーションの会社です。</h2>
+                <p class="sub-company__txt">名古屋市や春日井市、小牧市を中心とした地域密着の<br class="u-desktop">リフォーム・リノベーション会社・エムプラスです。<br>お客様との出会いとご縁、職人さんとの絆を大切にした<br class="u-desktop">まごころを込めたリフォームを実現します。</p>
             </div>
             <div class="sub-company__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/top/area/area-img.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
         </div>
