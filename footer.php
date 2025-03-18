7c22c38aa9bcdc36ddd5396e34222a0a0657661a
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-18 14:12:52 +0900

    メニューのアンカーリンク

--- footer.php
+++ footer.php
@@ -38,16 +38,16 @@
               <ul class="footer__contents">
                 <li>
                   <ul>
-                    <li class="footer__link"><a href="<?php echo esc_url(home_url("kitchen")); ?>">キッチンリフォーム</a></li>
-                    <li class="footer__link"><a href="<?php echo esc_url(home_url("bath")); ?>">浴室リフォーム</a></li>
-                    <li class="footer__link"><a href="<?php echo esc_url(home_url("toilet")); ?>">トイレリフォーム</a></li>
+                    <li class="footer__link"><a href="<?php echo esc_url(home_url("reform-menu/#kitchen")); ?>">キッチンリフォーム</a></li>
+                    <li class="footer__link"><a href="<?php echo esc_url(home_url("reform-menu/#bath")); ?>">浴室リフォーム</a></li>
+                    <li class="footer__link"><a href="<?php echo esc_url(home_url("reform-menu/#toilet")); ?>">トイレリフォーム</a></li>
                   </ul>
                 </li>
                 <li>
                   <ul>
-                    <li class="footer__link"><a href="<?php echo esc_url(home_url("washroom")); ?>">洗面リフォーム</a></li>
-                    <li class="footer__link"><a href="<?php echo esc_url(home_url("interior")); ?>">内装リフォーム</a></li>
-                    <li class="footer__link"><a href="<?php echo esc_url(home_url("exterior")); ?>">外装リフォーム</a></li>
+                    <li class="footer__link"><a href="<?php echo esc_url(home_url("reform-menu/#washroom")); ?>">洗面リフォーム</a></li>
+                    <li class="footer__link"><a href="<?php echo esc_url(home_url("reform-menu/#interior")); ?>">内装リフォーム</a></li>
+                    <li class="footer__link"><a href="<?php echo esc_url(home_url("reform-menu/#exterior")); ?>">外装リフォーム</a></li>
                   </ul>
                 </li>
               </ul>
@@ -55,16 +55,16 @@
             <li class="footer__item">
               <p class="footer__subTl">リフォームの流れ</p>
               <ul class="footer__contents">
-                <li class="footer__link"><a href="<?php echo esc_url(home_url("company")); ?>">ご依頼までの流れ</a></li>
+                <li class="footer__link"><a href="<?php echo esc_url(home_url("flow")); ?>">ご依頼までの流れ</a></li>
               </ul>
             </li>
             <li class="footer__item">
               <p class="footer__subTl">会社情報</p>
               <ul class="footer__contents">
-                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company")); ?>">代表挨拶</a></li>
-                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company")); ?>">経営理念</a></li>
-                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company")); ?>">会社概要</a></li>
-                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company")); ?>">アクセス</a></li>
+                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company/#greeting")); ?>">代表挨拶</a></li>
+                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company/#concept")); ?>">経営理念</a></li>
+                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company/#overview")); ?>">会社概要</a></li>
+                  <li class="footer__link"><a href="<?php echo esc_url(home_url("company/#access")); ?>">アクセス</a></li>
               </ul>
             </li>
             <li class="footer__item">
--- page-reform-menu.php
+++ page-reform-menu.php
@@ -65,127 +65,139 @@ $mainviews = [
         <div class="sub-menu__body">
             <ul class="sub-menu__list">
                 <li class="sub-menu__item" id="kitchen">
-                    <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/1.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="sub-menu__contents">
-                        <h2 class="sub-menu__title">キッチンリフォーム</h2>
-                        <div class="sub-menu__main">
-                            <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
-                            <div class="sub-menu__meta">
-                                <p class="sub-menu__price">45</p>
-                                <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                    <article class="sub-menu__box">
+                        <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/1.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                        <div class="sub-menu__contents">
+                            <h2 class="sub-menu__title">キッチンリフォーム</h2>
+                            <div class="sub-menu__main">
+                                <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
+                                <div class="sub-menu__meta">
+                                    <p class="sub-menu__price">45</p>
+                                    <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                                </div>
+                            </div>
+                            <div class="sub-menu__bottom">
+                                <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
+                                <ul class="sub-menu__checks">
+                                    <li class="sub-menu__check"><p class="sub-menu__note">使いにくい、せまい、掃除がしにくい。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">古い流し台から、嫌なにおいがする。</p></li>
+                                </ul>
                             </div>
                         </div>
-                        <div class="sub-menu__bottom">
-                            <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
-                            <ul class="sub-menu__checks">
-                                <li class="sub-menu__check"><p class="sub-menu__note">使いにくい、せまい、掃除がしにくい。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">古い流し台から、嫌なにおいがする。</p></li>
-                            </ul>
-                        </div>
-                    </div>
+                    </article>
                 </li>
                 <li class="sub-menu__item" id="bath">
-                    <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/2.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="sub-menu__contents">
-                        <h2 class="sub-menu__title">浴室リフォーム</h2>
-                        <div class="sub-menu__main">
-                            <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
-                            <div class="sub-menu__meta">
-                                <p class="sub-menu__price">42</p>
-                                <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                    <article class="sub-menu__box">
+                        <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/2.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                        <div class="sub-menu__contents">
+                            <h2 class="sub-menu__title">浴室リフォーム</h2>
+                            <div class="sub-menu__main">
+                                <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
+                                <div class="sub-menu__meta">
+                                    <p class="sub-menu__price">42</p>
+                                    <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                                </div>
+                            </div>
+                            <div class="sub-menu__bottom">
+                                <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
+                                <ul class="sub-menu__checks sub-menu__checks--wrap">
+                                    <li class="sub-menu__check"><p class="sub-menu__note">カビが気になる。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">節水したい。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">浴室が寒い。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">換気が気になる。</p></li>
+                                </ul>
                             </div>
                         </div>
-                        <div class="sub-menu__bottom">
-                            <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
-                            <ul class="sub-menu__checks sub-menu__checks--wrap">
-                                <li class="sub-menu__check"><p class="sub-menu__note">カビが気になる。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">節水したい。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">浴室が寒い。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">換気が気になる。</p></li>
-                            </ul>
-                        </div>
-                    </div>
+                    </article>
                 </li>
                 <li class="sub-menu__item" id="toilet">
-                    <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/3.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="sub-menu__contents">
-                        <h2 class="sub-menu__title">トイレリフォーム</h2>
-                        <div class="sub-menu__main">
-                            <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
-                            <div class="sub-menu__meta">
-                                <p class="sub-menu__price">14</p>
-                                <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                    <article class="sub-menu__box">
+                        <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/3.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                        <div class="sub-menu__contents">
+                            <h2 class="sub-menu__title">トイレリフォーム</h2>
+                            <div class="sub-menu__main">
+                                <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
+                                <div class="sub-menu__meta">
+                                    <p class="sub-menu__price">14</p>
+                                    <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                                </div>
+                            </div>
+                            <div class="sub-menu__bottom">
+                                <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
+                                <ul class="sub-menu__checks">
+                                    <li class="sub-menu__check"><p class="sub-menu__note">においや汚れが目立つ。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">和式から洋式に変えたい。</p></li>
+                                </ul>
                             </div>
                         </div>
-                        <div class="sub-menu__bottom">
-                            <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
-                            <ul class="sub-menu__checks">
-                                <li class="sub-menu__check"><p class="sub-menu__note">においや汚れが目立つ。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">和式から洋式に変えたい。</p></li>
-                            </ul>
-                        </div>
-                    </div>
+                    </article>
                 </li>
                 <li class="sub-menu__item" id="washroom">
-                    <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/4.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="sub-menu__contents">
-                        <h2 class="sub-menu__title">洗面リフォーム</h2>
-                        <div class="sub-menu__main">
-                            <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
-                            <div class="sub-menu__meta">
-                                <p class="sub-menu__price">12</p>
-                                <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                    <article class="sub-menu__box">
+                        <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/4.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                        <div class="sub-menu__contents">
+                            <h2 class="sub-menu__title">洗面リフォーム</h2>
+                            <div class="sub-menu__main">
+                                <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
+                                <div class="sub-menu__meta">
+                                    <p class="sub-menu__price">12</p>
+                                    <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                                </div>
+                            </div>
+                            <div class="sub-menu__bottom">
+                                <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
+                                <ul class="sub-menu__checks sub-menu__checks--wrap">
+                                    <li class="sub-menu__check"><p class="sub-menu__note">さびが気になる。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">水はけが気になる。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">排水溝がにおう。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">節水したい。</p></li>
+                                </ul>
                             </div>
                         </div>
-                        <div class="sub-menu__bottom">
-                            <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
-                            <ul class="sub-menu__checks sub-menu__checks--wrap">
-                                <li class="sub-menu__check"><p class="sub-menu__note">さびが気になる。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">水はけが気になる。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">排水溝がにおう。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">節水したい。</p></li>
-                            </ul>
-                        </div>
-                    </div>
+                    </article>
                 </li>
                 <li class="sub-menu__item" id="interior">
-                    <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/5.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="sub-menu__contents">
-                        <h2 class="sub-menu__title">内装リフォーム</h2>
-                        <div class="sub-menu__main">
-                            <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
-                            <div class="sub-menu__meta">
-                                <p class="sub-menu__price">35</p>
-                                <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                    <article class="sub-menu__box">
+                        <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/5.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                        <div class="sub-menu__contents">
+                            <h2 class="sub-menu__title">内装リフォーム</h2>
+                            <div class="sub-menu__main">
+                                <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
+                                <div class="sub-menu__meta">
+                                    <p class="sub-menu__price">35</p>
+                                    <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                                </div>
+                            </div>
+                            <div class="sub-menu__bottom">
+                                <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
+                                <ul class="sub-menu__checks">
+                                    <li class="sub-menu__check"><p class="sub-menu__note">壁紙や床板の張り替えしたい。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">和室を洋室に変えたい。</p></li>
+                                </ul>
                             </div>
                         </div>
-                        <div class="sub-menu__bottom">
-                            <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
-                            <ul class="sub-menu__checks">
-                                <li class="sub-menu__check"><p class="sub-menu__note">壁紙や床板の張り替えしたい。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">和室を洋室に変えたい。</p></li>
-                            </ul>
-                        </div>
-                    </div>
+                    </article>
                 </li>
                 <li class="sub-menu__item" id="exterior">
-                    <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/6.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
-                    <div class="sub-menu__contents">
-                        <h2 class="sub-menu__title">外装リフォーム</h2>
-                        <div class="sub-menu__main">
-                            <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
-                            <div class="sub-menu__meta">
-                                <p class="sub-menu__price">70</p>
-                                <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                    <article class="sub-menu__box">
+                        <div class="sub-menu__img"><img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/reform-menu/6.webp" alt="" width="" height="" loading="lazy" decoding="async"></div>
+                        <div class="sub-menu__contents">
+                            <h2 class="sub-menu__title">外装リフォーム</h2>
+                            <div class="sub-menu__main">
+                                <h4 class="sub-menu__subTl">工事費込<br>［総額］</h4>
+                                <div class="sub-menu__meta">
+                                    <p class="sub-menu__price">70</p>
+                                    <div class="sub-menu__txt">万円～<span>(税込)</span></div>
+                                </div>
+                            </div>
+                            <div class="sub-menu__bottom">
+                                <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
+                                <ul class="sub-menu__checks">
+                                    <li class="sub-menu__check"><p class="sub-menu__note">壁のひび割れが気になる。</p></li>
+                                    <li class="sub-menu__check"><p class="sub-menu__note">遮熱工事がしたい。</p></li>
                             </div>
                         </div>
-                        <div class="sub-menu__bottom">
-                            <h3 class="sub-menu__chechTl">リフォーム検討チェックリスト</h3>
-                            <ul class="sub-menu__checks">
-                                <li class="sub-menu__check"><p class="sub-menu__note">壁のひび割れが気になる。</p></li>
-                                <li class="sub-menu__check"><p class="sub-menu__note">遮熱工事がしたい。</p></li>
-                        </div>
-                    </div>
+                    </article>
                 </li>
             </ul>
         </div>
