8f41554ae328150ac753a862da1b1ea165585f54
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-20 15:15:40 +0900

    サンクスSP

--- page-thanks.php
+++ page-thanks.php
@@ -15,8 +15,8 @@ $mainviews = [
 
 <div class="thanks">
     <div class="thanks__inner inner">
-        <h2 class="thanks__title">お問い合わせありがとうございました。</h2>
-        <p class="thanks__txt">ご返答までにお時間をいただく場合があります。<br>ご了承いただけますと幸いです。 </p>
+        <h2 class="thanks__title">お問い合わせ<br class="u-mobile">ありがとうございました。</h2>
+        <p class="thanks__txt">ご返答までにお時間をいただく場合があります。<br class="u-desktop">ご了承いただけますと幸いです。 </p>
         <div class="thanks__btn"><a href="<?php echo esc_url(home_url()); ?>" class="btn">トップへ戻る</a></div>
     </div>
 </div>
